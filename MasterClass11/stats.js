8d6594b9b50bb7dc3730bb2e4d38f1e85058ec4b
Lincoln Ferreira <lincoln.carmo@sptech.school>
2022-04-26 13:23:38 -0300

    aprendendo outros metodos na biblioteca 'os'.

--- MasterClass11/stats.js
+++ MasterClass11/stats.js
@@ -6,13 +6,14 @@
     const memoriaLivre = parseInt(freemem() / 1024 / 1024);
     const memoriaTotal = parseInt(totalmem() / 1024 / 1024);
     const percentualMemoria = parseInt((memoriaLivre / memoriaTotal) * 100);
-
+    const cpu = os.cpus()[0];
     const status = {
         livre: `${memoriaLivre} MB`,
         total: `${memoriaTotal} MB`,
-        percentual: `${percentualMemoria}%`
+        percentual: `${percentualMemoria}%`,
     }
 
 
-    console.log(memoriaLivre, memoriaTotal, percentualMemoria);
+    // console.log(memoriaLivre, memoriaTotal, percentualMemoria, os.cpus()[0] );
     console.table(status);
+    console.table(cpu);
